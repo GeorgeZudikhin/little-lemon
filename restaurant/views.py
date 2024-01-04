38d4430489b5e41f5019973fef204e28ef60c443
Yehor Zudikhin <yehorzudikhin17@gmail.com>
2024-01-04 07:10:57 +0100

    Menu API & Models created

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    # The ListCreateAPIView automatically provides 'get' and 'post' methods
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    # The RetrieveUpdateDestroyAPIView automatically provides 'get', 'put', and 'delete' methods
